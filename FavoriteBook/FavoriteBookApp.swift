a944ff6826cbbb72b17b78517fb8274a426d5f9c
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-04-26 16:55:33 -0600

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Italo Stuardo on 26/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
